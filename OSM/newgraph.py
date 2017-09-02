49e3cbbf523e8998fa8d8705063282e99bb05421
Mateus <Mateus.rioos@gmail.com>
2017-09-02 20:56:09 -0300

    nova entrada

--- OSM/newgraph.py
+++ OSM/newgraph.py
@@ -197,10 +197,10 @@ class OSM:
         self.ways = new_ways
 
 # Gera arquivo com valores de exemplo
-d1Lat = -22.917487
-d1Lon = -43.303814
-d2Lat = -22.875423
-d2Lon = -43.219013
+d1Lat = -23.009872
+d1Lon = -43.655205
+d2Lat = -22.749231
+d2Lon = -43.180046
 
 G=le_arquivo(download_osm(d1Lon,d1Lat,d2Lon,d2Lat,"motorway|trunk|primary|secondary|tertiary|unclassified|track|service|residential"))
 
