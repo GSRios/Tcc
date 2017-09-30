458fc6aade5992a190894265778b5dc9beac4317
Mateus <Mateus.rioos@gmail.com>
2017-09-29 21:59:24 -0300

    c

--- OSM/graph2Cypher.py
+++ OSM/graph2Cypher.py
@@ -20,20 +20,20 @@ def graph2Cypher(aGraph):
     graph = Graph('ec2-34-223-213-240.us-west-2.compute.amazonaws.com:7474/db/data')
 
     #Partially generate the node representations
-    ##for aNode in aGraph.nodes(data = True):
+    for aNode in aGraph.nodes(data = True):
         #Generate a node identifier for Cypher
-     ##   varName = getRndTag(2)+getRndTag(2,dct=numDCT)
+        varName = getRndTag(2)+getRndTag(2,dct=numDCT)
         #Append the node's ID attribute so that the node-ID information used by Networkx is preserved.
-     ##   nodeItems = [("ID","%s" % aNode[0])]
-     ##   nodeItems.extend(aNode[1].items())
+        nodeItems = [("ID","%s" % aNode[0])]
+        nodeItems.extend(aNode[1].items())
         #Create the key-value representation of the node's attributes taking care to add quotes when the value is of type string        
-     ##   nodeAttributes = "{%s}" % ",".join(map(lambda x:"%s:%s" %(x[0],x[1]) if not type(x[1])==str else "%s:'%s'" %(x[0],x[1]) ,nodeItems))
+        nodeAttributes = "{%s}" % ",".join(map(lambda x:"%s:%s" %(x[0],x[1]) if not type(x[1])==str else "%s:'%s'" %(x[0],x[1]) ,nodeItems))
         #Store it to a dictionary indexed by the node-id.
         #nodeStatements[aNode[0]] = [varName, "(%s %s)" % (varName, nodeAttributes)]
         #i_node = Node(varName, nodeAttributes)
-     ##   print("Inserindo %s" % varName)
+        print("Inserindo %s" % varName)
         #print(str('create (%s %s)' % varName, nodeAttributes)) 
-     ##   graph.run("create (%s %s)" % (varName, nodeAttributes))
+        graph.run("create (%s %s)" % (varName, nodeAttributes))
         
     #Generate the relationship representations
     for anEdge in aGraph.edges(data = True):
@@ -43,9 +43,8 @@ def graph2Cypher(aGraph):
             edgeAttributes = "{%s}" % ",".join(map(lambda x:"%s:%s" %(x[0],x[1]) if not type(x[1])==str else "%s:'%s'" %(x[0],x[1]) ,edgeItems))
         #NOTE: Declare the links by their Cypher node-identifier rather than their Networkx node identifier
         #edgeStatements.append("(%s)-[:LINKED_TO %s]->(%s)" % (nodeStatements[anEdge[0]][0], edgeAttributes, nodeStatements[anEdge[1]][0]))
-        print (edgeItems)
-       ##print("create (%s)-[:LINKED_TO %s]->(%s)" % (nodeStatements[anEdge[0]][0], edgeAttributes, nodeStatements[anEdge[1]][0]))
-        #graph.run(str("create (%s)-[:LINKED_TO %s]->(%s)" % (nodeStatements[anEdge[0]][0], edgeAttributes, nodeStatements[anEdge[1]][0])))
+        print("create (%s)-[:LINKED_TO %s]->(%s)" % (nodeStatements[anEdge[0]][0], edgeAttributes, nodeStatements[anEdge[1]][0]))
+        graph.run("create (%s)-[:LINKED_TO %s]->(%s)" % (nodeStatements[anEdge[0]][0], edgeAttributes, nodeStatements[anEdge[1]][0]))
         
     #Put both definitions together and return the create statement.
     #return "create %s,%s;\n" % (",".join(map(lambda x:x[1][1],nodeStatements.items())),",".join(edgeStatements))
