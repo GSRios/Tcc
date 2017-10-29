999d157c8e888aa8234c15ec30e7ec5d53f8cb4c
Mateus <Mateus.rioos@gmail.com>
2017-10-29 01:09:43 -0200

    Dijkstra

--- Rotas/algorithms.py
+++ Rotas/algorithms.py
@@ -8,5 +8,5 @@ def get_dijkstra(minLat, minLon, maxLat, maxLon):
     graph = Graph("http://localhost:7474/db/data/")
     query = 'MATCH (from:Node) WHERE from.Lat = \'%s\' AND from.Long = \'%s\'MATCH (to:Node) WHERE to.Lat = \'%s\' AND to.Long = \'%s\'CALL apoc.algo.dijkstra(from, to, \'LINKED_TO\', \'Weight\') yield path as path, weight as weight RETURN path, weight' % (minLat, maxLat, minLon, maxLon)
     ret = graph.run(query)
-    return ret
+    return ret.data()
     
