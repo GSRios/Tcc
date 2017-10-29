d033fe0555d9eedc804d1671c5279f52809c666b
Mateus <Mateus.rioos@gmail.com>
2017-10-29 01:32:01 -0200

    Dijkstra

--- Rotas/algorithms.py
+++ Rotas/algorithms.py
@@ -6,7 +6,7 @@ def get_dijkstra(minLat, minLon, maxLat, maxLon):
     authenticate("localhost:7474", "neo4j", "tccneo4j2017")
     # connect to authenticated graph database
     graph = Graph("http://localhost:7474/db/data/")
-    query = 'MATCH (from:Node) WHERE from.Lat = \'%s\' AND from.Long = \'%s\'MATCH (to:Node) WHERE to.Lat = \'%s\' AND to.Long = \'%s\'CALL apoc.algo.dijkstra(from, to, \'LINKED_TO\', \'Weight\') yield path as path, weight as weight RETURN path, weight' % (minLat, maxLat, minLon, maxLon)
+    query = 'MATCH (from:Node) WHERE from.Lat = \'%s\' AND from.Long = \'%s\'MATCH (to:Node) WHERE to.Lat = \'%s\' AND to.Long = \'%s\'CALL apoc.algo.dijkstra(from, to, \'LINKED_TO\', \'Weight\') yield path as path, weight as weight RETURN path,weight, from, to' % (minLat, maxLat, minLon, maxLon)    
     ret = graph.run(query)
     return ret.data()
     
