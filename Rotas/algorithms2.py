cf9e46640584d955eb734ccf67efed38ffca921f
Mateus <Mateus.rioos@gmail.com>
2017-10-29 21:26:39 -0200

    Algs

--- Rotas/algorithms2.py
+++ Rotas/algorithms2.py
@@ -4,6 +4,9 @@ import networkx as nx
 from subprocess import call
 from coordinates import get_bounding_box
 
+
+file = '/var/lib/neo4j/data/bbox.graphml' 
+
 def distancia(lon1, lat1, lon2, lat2):
     #Converte de Decimal para Radianos
     lon1, lat1, lon2, lat2 = map(radians, [float(lon1), float(lat1), float(lon2), float(lat2)])
@@ -18,10 +21,7 @@ def distancia(lon1, lat1, lon2, lat2):
     return m  
 
 
-#Local do arquivo onde serão salvos os pontos contidos no bounding box
-file = '/var/lib/neo4j/data/alg_bbox.graphml'
-
-def nearest_node(lat,lon):
+def nearest_node(G,lat,lon):
     menor = 9999
     for i in G.nodes():
         dist_node = distancia(lat, lon, G.node[i]['Lat'], G.node[i]['Long'])
@@ -34,8 +34,8 @@ def get_dijikstra_path(minLat, minLon ,maxLat, maxLon):
     get_bounding_box(maxLat, maxLon, minLat, minLon)
     G = nx.read_graphml(file)
     return_list = []
-    start_node = nearest_node(minLat, minLon)
-    end_node = nearest_node(maxLat, maxLon)
+    start_node = nearest_node(G,minLat, minLon)
+    end_node = nearest_node(G,maxLat, maxLon)
     SP = nx.dijkstra_path(G,start_node, end_node, weight='Weight')
     for i in SP:
         return_list.append([float(G.node[i]['Lat']),float(G.node[i]['Long'])])
@@ -44,11 +44,12 @@ def get_dijikstra_path(minLat, minLon ,maxLat, maxLon):
 
 
 def get_a_start(minLat, minLon ,maxLat, maxLon):
+    #Local do arquivo onde serão salvos os pontos contidos no bounding box      
     get_bounding_box(maxLat, maxLon, minLat, minLon)
     G = nx.read_graphml(file)
     return_list = []
-    start_node = nearest_node(minLat, minLon)
-    end_node = nearest_node(maxLat, maxLon)
+    start_node = nearest_node(G,minLat, minLon)
+    end_node = nearest_node(G,maxLat, maxLon)
     SP = nx.astar_path(G,start_node, end_node, weight='Weight')
     for i in SP:
         return_list.append([float(G.node[i]['Lat']),float(G.node[i]['Long'])])
